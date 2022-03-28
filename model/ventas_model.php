4474980d7cdc2afbc725aca7a59ce0774a8707a2
Stondark <86988062+Stondark@users.noreply.github.com>
2022-03-28 13:05:06 -0500

    Add files via upload

---
+++ model/ventas_model.php
@@ -0,0 +1,88 @@
+<?php
+    require("../config/db.php");
+    class Ventas extends Conexion{
+        public function get_ventas(){
+            parent::conectar();
+            $sql = "SELECT * FROM ventas";
+            $consulta = $this->conexion->prepare($sql);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+            //echo json_encode($resultado);
+            //var_dump($resultado); // Mostramos el array de la consulta en pantalla
+        }
+
+        public function get_ventas_id($venta_id){
+            parent::conectar();
+            $sql = "SELECT * FROM ventas WHERE id = ?";
+            $consulta = $this->conexion->prepare($sql);
+            $consulta->bindValue(1, $venta_id);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+        }
+
+        public function delete_ventas($venta_id){
+            parent::conectar();
+            $sql = "DELETE FROM ventas WHERE id = ?";
+            $consulta = $this->conexion->prepare($sql);
+            $consulta->bindValue(1, $venta_id);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+        }
+
+        public function insert_venta($venta_nombre, $venta_numero, $venta_direccion, $venta_fecha, $venta_producto, $venta_cantidad, $venta_envio, $venta_vendedor, $venta_estado, $venta_total){
+            parent::conectar();
+            $sql = "INSERT INTO ventas(nombre_cliente,numero,direccion,fecha_entrega,producto,cantidad,id_tipoenvio,id_vendedor,id_estado,total) VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";   
+            $consulta = $this->conexion->prepare($sql);
+            $consulta->bindValue(1, $venta_nombre);
+            $consulta->bindValue(2, $venta_numero);
+            $consulta->bindValue(3, $venta_direccion);
+            $consulta->bindValue(4, $venta_fecha);
+            $consulta->bindValue(5, $venta_producto);
+            $consulta->bindValue(6, $venta_cantidad);
+            $consulta->bindValue(7, $venta_envio);
+            $consulta->bindValue(8, $venta_vendedor);
+            $consulta->bindValue(9, $venta_estado);
+            $consulta->bindValue(10, $venta_total);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+        }
+
+        public function update_venta($venta_id, $venta_nombre, $venta_numero, $venta_direccion, $venta_fecha, $venta_producto, $venta_cantidad, $venta_envio, $venta_vendedor, $venta_estado, $venta_total){
+            parent::conectar();
+            $sql = "UPDATE venta SET nombre_cliente = ?, numero = ?, direccion = ?, fecha_entrega = ?, producto = ?, cantidad = ?, id_tipoenvio = ?, id_vendedor = ?, id_estado = ?, total = ?
+                    WHERE id = ?";
+            $consulta = $this->conexion->prepare($sql);
+            $consulta->bindValue(1, $venta_nombre);
+            $consulta->bindValue(2, $venta_numero);
+            $consulta->bindValue(3, $venta_direccion);
+            $consulta->bindValue(4, $venta_fecha);
+            $consulta->bindValue(5, $venta_producto);
+            $consulta->bindValue(6, $venta_cantidad);
+            $consulta->bindValue(7, $venta_envio);
+            $consulta->bindValue(8, $venta_vendedor);
+            $consulta->bindValue(9, $venta_estado);
+            $consulta->bindValue(10, $venta_total);
+            $consulta->bindValue(11, $venta_id);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+        }
+
+        public function count_venta(){
+            parent::conectar();
+            $sql = "SELECT * FROM ventas";
+            $consulta = $this->conexion->prepare($sql);
+            $consulta->execute();
+            $resultado = $consulta->rowCount();
+            //var_dump($resultado);
+            return $resultado;
+
+        }
+
+    }
+
+?>
---
+++ views/productos.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/productos.css" />
+    <link rel="stylesheet" href="//cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <title>Productos</title>
+    <!-- Include nav -->
+    <?php include_once("../includes/nav.php");
+        
+    ?>
+</head>
+<body>
+    <!-- Contenedor principal -->
+    <div class="container-productos">
+        <h1 class="title">Productos</h1>
+        <!-- Inicio tabla --->
+        <div class="tabla-productos">
+            <table id="lista-productos">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>PRODUCTO</th>
+                        <th>CANTIDAD</th>
+                        <th>PRECIO</th>
+                        <th></th>
+                    </tr>
+                </thead>
+            </table>
+        </div> 
+        <!-- Botón de nueva venta --->
+        <div class="button-new">
+            <button id="abrir-venta">
+                <a href="#">
+                    Nuevo producto <i class="fa-solid fa-plus"></i>
+                </a>
+            </button>
+        </div>
+    </div>
+
+
+    <!-- Modal nueva venta -->
+
+    <div class="modal-container" id="modal-container">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2>Nuevo producto</h2>
+                <a class="close" id="close">X</a>
+            </div>
+            <div class="modal-form">
+                <form action="" method="post" id="form-new-producto">
+                    <label for="">PRODUCTO</label>
+                    <input type="text" id="producto" name="producto">
+                    <label for="">CANTIDAD</label>
+                    <input type="text" id="cantidad" name="cantidad">
+                    <label for="">PRECIO</label>
+                    <input type="text" id="precio" name="precio">
+                    <button>Añadir</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!-- DataTables --->
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <!-- Jquery validation lib-->
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js"></script>
+    <!-- Sweet Alert-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- JS -->
+    <script src="../js/index.js"></script>
+    <script src="../js/productoscript.js"></script>
+    <script src="../libs/jqueryvalidation/validation.js"></script>
+</body>
+
+</html>
