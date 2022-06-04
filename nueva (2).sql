847f9c65c57f24e3248fafe4aa031828751d33a1
Daniel Forigua <forigua.daniel@gmail.com>
2022-06-03 22:30:14 -0500

    actualizacion base de datos

--- nueva (2).sql
+++ nueva (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-06-2022 a las 04:19:08
+-- Tiempo de generación: 04-06-2022 a las 05:23:59
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.10
 
@@ -36,6 +36,14 @@ CREATE TABLE `clientes` (
   `numero` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nombre`, `cedula`, `direccion`, `correo`, `numero`) VALUES
+(8, 'Juanito', '15674', 'cra123', 'juanito@gmail.com', '123456'),
+(9, 'Pepito', '54276', 'av123', 'pepito@aol.com', '31188888');
+
 -- --------------------------------------------------------
 
 --
@@ -56,7 +64,9 @@ CREATE TABLE `productos` (
 INSERT INTO `productos` (`id_producto`, `producto`, `cantidad`, `precio_venta`) VALUES
 (1, 'papas', 2, 2000),
 (2, 'mentas', 5, 1000),
-(8, 'coca cola', 1, 2000);
+(3, 'coca cola', 1, 2000),
+(4, 'papas', 2, 3500),
+(5, 'chocorramo', 1, 1500);
 
 -- --------------------------------------------------------
 
@@ -68,10 +78,17 @@ CREATE TABLE `ventas` (
   `id_venta` int(11) NOT NULL,
   `fecha_entrega` date NOT NULL,
   `id_cliente` int(11) NOT NULL,
-  `medio_pago` varchar(30) NOT NULL,
-  `estado_envio` varchar(15) NOT NULL,
-  `tipo_envio` varchar(12) NOT NULL
+  `id_envio` int(11) NOT NULL,
+  `id_pago` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`id_venta`, `fecha_entrega`, `id_cliente`, `id_envio`, `id_pago`) VALUES
+(36, '2022-07-05', 8, 1, 3),
+(37, '2022-06-04', 9, 2, 4);
 
 -- --------------------------------------------------------
 
@@ -84,6 +101,16 @@ CREATE TABLE `ventas_productos` (
   `id_productos` int(11) NOT NULL,
   `id_ventas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ventas_productos`
+--
+
+INSERT INTO `ventas_productos` (`id_ventasproducto`, `id_productos`, `id_ventas`) VALUES
+(4, 5, 36),
+(5, 3, 36),
+(6, 4, 36),
+(7, 2, 36);
 
 --
 -- Índices para tablas volcadas
@@ -109,7 +136,9 @@ ALTER TABLE `ventas`
   ADD PRIMARY KEY (`id_venta`),
   ADD UNIQUE KEY `id_cliente` (`id_cliente`),
   ADD KEY `id_vendedor_3` (`id_cliente`),
-  ADD KEY `id_cliente_2` (`id_cliente`);
+  ADD KEY `id_cliente_2` (`id_cliente`),
+  ADD KEY `envio_fk` (`id_envio`),
+  ADD KEY `pagos_fk` (`id_pago`);
 
 --
 -- Indices de la tabla `ventas_productos`
@@ -127,25 +156,25 @@ ALTER TABLE `ventas_productos`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas_productos`
 --
 ALTER TABLE `ventas_productos`
-  MODIFY `id_ventasproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_ventasproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
@@ -155,7 +184,9 @@ ALTER TABLE `ventas_productos`
 -- Filtros para la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  ADD CONSTRAINT `usuarios_fk` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`);
+  ADD CONSTRAINT `cliente_fk` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
+  ADD CONSTRAINT `envio_fk` FOREIGN KEY (`id_envio`) REFERENCES `envios` (`id_envio`),
+  ADD CONSTRAINT `pagos_fk` FOREIGN KEY (`id_pago`) REFERENCES `pagos` (`id_pago`);
 
 --
 -- Filtros para la tabla `ventas_productos`
